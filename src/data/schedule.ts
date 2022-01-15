4bdd45a12fc6043247a6d495d8cc6bb4300cf749
Samarth Goel <samarthgoel@Samarths-MacBook-Air.local>
2022-01-14 17:56:41 -0800

    update schedule

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -18,12 +18,46 @@ const schedule: Lesson[] = [
     date: 'Feb 8',
     format: 'Lecture 2',
     topic: 'HTML',
+    readingTitle: 'HTML',
+    readingLink: '/docs/Lessons/Lesson2',
   },
   {
     week: 1,
     date: 'Feb 10',
     format: 'Lecture 3',
     topic: 'CSS 1',
+    readingTitle: 'Introduction to CSS',
+    readingLink: '/docs/Lessons/Lesson3',
+  },
+
+  {
+    week: 2,
+    date: 'Feb 15',
+    format: 'Lecture 4',
+    topic: 'CSS 2',
+    readingTitle: 'Advanced CSS',
+    readingLink: '/docs/Lessons/Lesson4',
+  },
+  {
+    week: 2,
+    date: 'Feb 17',
+    format: 'Lecture 5',
+    topic: 'JavaScript 1',
+    readingTitle: 'Basic JavaScript',
+    readingLink: '/docs/Lessons/Lesson5',
+  },
+
+  {
+    week: 3,
+    date: 'Feb 22',
+    format: 'Lecture 5',
+    topic: 'JavaScript 2',
+  },
+  {
+    week: 3,
+    date: 'Feb 24',
+    format: 'Lecture 6',
+    topic: 'JavaScript 3',
   }
 ];
 
