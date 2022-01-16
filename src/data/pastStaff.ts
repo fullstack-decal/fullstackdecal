e65c4555f38f0546eacfbdccb898b6f99ca03947
Samarth Goel <samarthgoel@Samarths-MacBook-Air.local>
2022-01-16 00:44:42 -0800

    update past staff linkedins

--- src/data/pastStaff.ts
+++ src/data/pastStaff.ts
@@ -2,27 +2,27 @@ const staff = [
   {
     name: 'Arushi Somani',
     image: '/img/past-staff/arushi.png',
-    linkedin: 'https://www.linkedin.com/in/caelinsutch/',
+    linkedin: 'https://www.linkedin.com/in/arushisomani/',
   },
   {
     name: 'Hector Ramos',
     image: '/img/past-staff/hector.png',
-    linkedin: 'https://www.linkedin.com/in/jialin-wu-658929185/',
+    linkedin: 'https://www.linkedin.com/in/hector-ramos-/',
   },
   {
     name: 'Ervin Baccay',
     image: '/img/past-staff/ervin.png',
-    linkedin: 'https://www.linkedin.com/in/jialin-wu-658929185/',
+    linkedin: 'https://www.linkedin.com/in/ebaccay/',
   },
   {
     name: 'Abhi Wadekar',
     image: '/img/past-staff/abhi.png',
-    linkedin: 'https://www.linkedin.com/in/jialin-wu-658929185/',
+    linkedin: 'https://www.linkedin.com/in/abhiraam-wadekar/',
   },
   {
     name: 'Brian Holt',
     image: '/img/past-staff/brian.png',
-    linkedin: 'https://www.linkedin.com/in/jialin-wu-658929185/',
+    linkedin: 'https://www.linkedin.com/in/btholt/',
   }
 ];
 
