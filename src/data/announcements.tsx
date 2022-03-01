08b5817d9ad7cb0f8dad597b0d2ec744fb0ad575
Samarth Goel <samarthgoel@Samarths-MacBook-Air.local>
2022-02-28 22:03:00 -0800

    announce 4

--- src/data/announcements.tsx
+++ src/data/announcements.tsx
@@ -4,13 +4,15 @@ import Week0 from './announcements/week0.mdx';
 import Week1 from './announcements/week1.mdx';
 import Week2 from './announcements/week2.mdx';
 import Week3 from './announcements/week3.mdx';
+import Week4 from './announcements/week4.mdx';
 
 const announcements = [
   <Recruiting key={0}/>,
   <Week0 key={1}/>,
   <Week1 key={2}/>,
   <Week2 key={3}/>,
-  <Week3 key={4}/>
+  <Week3 key={4}/>,
+  <Week4 key={5}/>
 ];
 
 export default announcements;
