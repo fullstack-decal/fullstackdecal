86290db75bb20e594e5ef376c14e0fc6db7a8faa
webatberkeley <112354265+webatberkeley@users.noreply.github.com>
2023-05-29 22:25:03 +0200

    Update staff.ts

--- src/data/staff.ts
+++ src/data/staff.ts
@@ -1,14 +1,5 @@
 const staff = [
   {
-    name: 'Forty Two',
-    image: '/img/staff/sebi.png',
-    role: 'Number',
-    linkedin: 'https://www.linkedin.com/school/42born2code/',
-    personal: 'https://www.fortytwo.zip/',
-    bio: 'Hi! I’m 42! Testing push privileges',
-    email: 'mailto: 42@berkeley.edu',
-  },
-  {
     name: 'Nitya Krishnakumar',
     image: '/img/staff/nitya.png',
     role: 'Facilitator',
