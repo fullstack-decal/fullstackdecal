1e93631ec291fea8720f74fb8a4ea606582cf1a3
Vihan <vihan+github@vihan.org>
2023-08-14 16:52:56 -0700

    Add redirection link
    Signed-off-by: Vihan <vihan+github@vihan.org>

--- src/pages/enroll.tsx
+++ src/pages/enroll.tsx
@@ -1,46 +1,17 @@
-import React from "react";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import { Box, Text, Container } from "@chakra-ui/react";
-import RecruitingResources from "../data/recruiting-resources.mdx";
-import DevResources from "../data/dev-resources.mdx";
-import PastIterations from "../data/past-iterations.mdx";
+import React, { useEffect } from "react";
+import { Center, Text } from "@chakra-ui/react";
 
-const Resources = () => {
-  const { siteConfig } = useDocusaurusContext();
+const Enroll = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://airtable.com/app4DQfVs1iDSqn4b/shrFGkWU1iF8nLpgS";
+  }, []);
 
   return (
-    <Layout
-      title={siteConfig.title}
-      description="Description will go into a meta tag in <head />"
-    >
-      <Box w="100vw" className="darkModeContainer">
-        <Container maxW="container.lg" pt={16} pb={20} px={[7, 10]}>
-          <Text textStyle="title2" color="blue" mb={[6]}>
-            ENROLL
-          </Text>
-          <Text style={{ fontSize: 33 }} mb={[50]}>
-            We're excited to have you join our Fall 2023 class!
-          </Text>
-          <Text style={{ fontSize: 25 }} mb={[100]}>
-            To apply please fill out the{" "}
-            <a
-              href="https://airtable.com/app4DQfVs1iDSqn4b/shrFGkWU1iF8nLpgS"
-              target="_blank"
-              rel="noreferrer"
-            >
-              application form.
-            </a>
-          </Text>
-
-          <Text textStyle="subtitle1" mt={2} mb={4}>
-            Past Course Iterations
-          </Text>
-          <PastIterations />
-        </Container>
-      </Box>
-    </Layout>
+    <Center w="100vw" h="100vh">
+      <Text>Redirecting...</Text>
+    </Center>
   );
 };
 
-export default Resources;
+export default Enroll;
