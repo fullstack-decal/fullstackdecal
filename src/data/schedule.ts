7c3efc557572ad8b47761eb41931c7fa421bb89b
oski <oski@berkeley.edu>
2026-08-13 10:36:00 -0500

    fall sem

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -273,17 +273,6 @@ const weeks: Week[] = [
     ],
   },
   {
-    title: "Spring Break",
-    lessons: [
-      {
-        topic: "No Lecture",
-      },
-      {
-        topic: "No Lecture"
-      },
-    ],
-  },
-  {
     title: "Databases, last piece of the puzzle",
     lessons: [
       {
@@ -388,6 +377,17 @@ const weeks: Week[] = [
     ],
   },
   {
+    title: "Thanksgiving Break",
+    lessons: [
+      {
+        topic: "No Lecture",
+      },
+      {
+        topic: "No Lecture",
+      },
+    ],
+  },
+  {
     title: "The final stretch!",
     lessons: [
       {
@@ -447,13 +447,13 @@ const vitamins: IncompleteAssignment[] = [
       name: "APIs and Servers",
     },
     assignedWeek: 5,
-    dueWeek: 7,
+    dueWeek: 6,
     dueDay: 2,
   },
   {
     link: { link: "/docs/Assignments/Vitamins/Databases", name: "Databases" },
-    assignedWeek: 7,
-    dueWeek: 8,
+    assignedWeek: 6,
+    dueWeek: 7,
     dueDay: 2,
   },
   {
@@ -461,17 +461,17 @@ const vitamins: IncompleteAssignment[] = [
       link: "/docs/Assignments/Vitamins/NextAuth",
       name: "Next.js and Authentication",
     },
-    assignedWeek: 8,
-    dueWeek: 9,
-    dueDay: 6,
+    assignedWeek: 7,
+    dueWeek: 8,
+    dueDay: 2,
   },
   {
     link: {
       link: "/docs/Assignments/Vitamins/CSSRedux",
       name: "CSS Frameworks and Redux",
     },
-    assignedWeek: 9,
-    dueWeek: 10,
+    assignedWeek: 8,
+    dueWeek: 9,
     dueDay: 6,
   },
   {
@@ -479,8 +479,8 @@ const vitamins: IncompleteAssignment[] = [
       link: "/docs/Assignments/Vitamins/PythonDeploy",
       name: "Flask, Django, and Deployment (EXTRA CREDIT/ATTENDANCE FIXER)",
     },
-    assignedWeek: 10,
-    dueWeek: 12,
+    assignedWeek: 9,
+    dueWeek: 11,
     dueDay: 1,
   },
 ];
@@ -501,7 +501,7 @@ const projects: IncompleteAssignment[] = [
       name: "APImagination",
     },
     assignedWeek: 4,
-    dueWeek: 8,
+    dueWeek: 7,
     dueDay: 2,
   },
   {
@@ -509,13 +509,13 @@ const projects: IncompleteAssignment[] = [
       link: "/docs/Assignments/Projects/FinalProject",
       name: "Final Project",
     },
-    assignedWeek: 7,
+    assignedWeek: 6,
     dueWeek: 11,
     dueDay: 2,
     checkpoints: [
       {
         name: "Project Idea + Design Prototype",
-        dueWeek: 9,
+        dueWeek: 8,
         dueDay: 5,
       },
     ],
@@ -526,8 +526,8 @@ const schedule: Schedule = {
   weeks,
   vitamins: resolveAssignments(vitamins, "Vitamin"),
   projects: resolveAssignments(projects, "Project"),
-  startDate: new Date("February 9, 2026"),
-  lessonDays: [2, 3],
+  startDate: new Date("September 13, 2026"),
+  lessonDays: [3, 4],
 };
 
 export default schedule;
