034db7a5bea7f426c25aded33c82bb0c1e5e1189
Sophie <sophie_luk@berkeley.edu>
2026-09-09 18:23:32 -0700

    remove notif

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -158,11 +158,11 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
 
-      announcementBar: {
-      id: "announcementBar-4",
-      content: `Applications are open! Apply <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/PqbLRBeTUDPhr7uV9">here</a>`,
-      isCloseable: true,
-    },
+    // announcementBar: {
+    //  id: "announcementBar-4",
+    //  content: `Applications are open! Apply <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/PqbLRBeTUDPhr7uV9">here</a>`,
+    //  isCloseable: true,
+    // },
     // announcementBar: {
     //   id: "announcementBar-3",
     //   content: `We still have some spots open for Spring 2026! Apply <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSfZpLvil5TvY2QNWlSenUTdOAgg-PKIdsaA6NFeKyCTmmskig/viewform">here</a>`,
